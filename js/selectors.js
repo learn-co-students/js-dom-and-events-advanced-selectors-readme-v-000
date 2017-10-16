eed5ef30456d9273c890b98dcd9dc622d248147d
smithwebtek <brad@smithwebtek.com>
2017-10-16 16:40:47 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
-// declare your functions here...
+$(function(){
+  console.log('ready freddy!')
+})
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  let last = $('img').length
+  let penultimate = $('img').length-1
+  return $('img').slice(penultimate, last)
+} 
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('div').slice(0,2)
+}
+
+function firstListItem() {
+  return $('#pic-list li').slice(0,1)
+}
+
