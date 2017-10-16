c7e27c0ec5008eb28daade45a886011ff9c5cc0e
smithwebtek <brad@smithwebtek.com>
2017-10-16 16:48:34 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -7,9 +7,11 @@ function paragraphSelector(){
 }
 
 function lastImageSelector(){
-  let last = $('img').length
-  let penultimate = $('img').length-1
-  return $('img').slice(penultimate, last)
+  // let last = $('img').length
+  // let penultimate = $('img').length-1
+  // return $('img').slice(penultimate, last)
+ 
+  return $('img:last')
 } 
 
 function ninjaBabySelector() {
@@ -17,10 +19,12 @@ function ninjaBabySelector() {
 }
 
 function divSelector(){
-  return $('div').slice(0,2)
+  // return $('div').slice(0,2)
+  return $('.pics')
 }
 
 function firstListItem() {
-  return $('#pic-list li').slice(0,1)
+  // return $('#pic-list li').slice(0,1)
+  return $('#pic-list li:first-child')
 }
 
