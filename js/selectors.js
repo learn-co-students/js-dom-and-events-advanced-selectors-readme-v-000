f125450d7c553352a2cf56f01eb8e65a0aa588e2
johnsontaiwo <josseytee@gmail.com>
2017-10-24 19:01:35 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraph = $('p')
+  return paragraph
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  var baby = $('#baby-ninja')
+  return baby
+}
+
+function divSelector() {
+  var pics = $('.pics')
+  return pics
+}
+
+function firstListItem() {
+  var firstItem = $('ul li:first-child')
+  return firstItem
+}
