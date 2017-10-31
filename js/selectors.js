91f87b0099bdb0ee17fe9783d02a289537eb5f18
veloblank <veloblank@gmail.com>
2017-10-31 04:40:15 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,19 @@
 function paragraphSelector() {
-  $("p")
+  return $("p")
+}
+
+function lastImageSelector() {
+  return $("div img:last")
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja")
+}
+
+function divSelector() {
+  return $(".pics")
+}
+
+function firstListItem() {
+  return $("ul li:first")
 }
