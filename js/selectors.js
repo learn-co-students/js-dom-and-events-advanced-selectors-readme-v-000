5f60dab73f65dd1ea5b68b43e3045c9eb15c5a8c
Joanna <jmgsese@gmail.com>
2017-12-01 15:41:24 -0600

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+paragraphSelector = () => {
+  return $('p')
+}
+
+lastImageSelector = () => {
+  return $('img:last')
+}
+
+ninjaBabySelector = () => {
+  return $('#baby-ninja')
+}
+
+divSelector = () => {
+  return $('.pics')
+}
+
+firstListItem = () => {
+  return $('#pic-list li:first-child')
+}
