42abcec765d5ccac475a63b9a3dec3337849be3d
jss530 <jsifre01@gmail.com>
2018-01-29 20:08:49 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  const paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  const lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  const baby = $('#baby-ninja')
+  return baby
+}
+
+function divSelector() {
+  const pictures = $('.pics')
+  return pictures
+}
+
+function firstListItem() {
+  const firstChild = $('#pic-list li:first-child')
+  return firstChild
+}
