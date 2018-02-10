36f6f6a3c03522b7d3c5881917d80cb6fd153be7
Oski <github email address>
2018-02-10 15:21:58 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+    return $("p")
+}
+
+function lastImageSelector(){
+    return $("#pic-list li:nth-child(3) img")
+}
+
+function ninjaBabySelector(){
+    return $("div.pics img:first")
+}
+
+function divSelector(){
+    return $(".pics");
+}
+
+function firstListItem(){
+    return $("ul#pic-list li:nth-child(1)");
+}
