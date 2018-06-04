9128661b2c6c720ba238e58426990124230b4511
Randall Robinson <keith_adams007@yahoo.com>
2018-06-04 00:44:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+ function paragraphSelector() {
+   const x = $('p')
+   return x
+ }
+ 
+ function lastImageSelector() {
+   const x = $('img:last')
+   return x
+ }
+ 
+ function ninjaBabySelector() {
+   return $('#baby-ninja')
+ }
+ 
+ function divSelector() {
+   return $('.pics')
+ }
+ 
+ function firstListItem() {
+   return $('ul# pic-list:first-child')
+ }
