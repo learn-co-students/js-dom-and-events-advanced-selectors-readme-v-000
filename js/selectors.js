5849d12a2785f4509adf09d89a28e970787701a2
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-06-04 13:10:57 -0700

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  const paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  const lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  const ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  const divs = $('.pics');
+  return divs;
+}
+
+function firstListItem() {
+  const firstListItem = $('ul#pic-list li:first-child');
+  return firstListItem;
+}
