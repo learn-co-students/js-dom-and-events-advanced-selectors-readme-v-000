a1f91e2c02d5cde75630679f919c921255c66912
ritchey0713 <ritchey0713@gmail.com>
2018-07-08 13:23:19 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('.pics #baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
