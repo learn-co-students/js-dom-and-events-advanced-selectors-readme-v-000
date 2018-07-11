309e59c0a37d0fa3b4faba25b71871e1a7572565
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-11 22:42:03 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 // declare your functions here...
+  return $('p');
+}
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector (){
+  return $('#baby-ninja');
+}
+
+function divSelector (){
+  return $('div .pics');
+}
+
+function firstListItem(){
+  return $('ul #pic-list:first-child');
+}
