5ee652cd2c653d5c02cf010277fbd43f7bce6751
Joseph Parker <jparker010190@gmail.com>
2018-07-13 15:45:57 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -10,8 +10,12 @@ function ninjaBabySelector(){
   return $('#baby-ninja')
 }
 function divSelector(){
+<<<<<<< HEAD
   return $('div').slice(0,2)
 }
 function firstListItem(){
   return $('ul li:first')
+=======
+  return $('div')
+>>>>>>> 33fff9c54ff21cd5b401f7b71ae9f1dd89b49079
 }
