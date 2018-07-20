b8da98e7904f8264a3ab47737d345bf27542e106
funemployeddad <funemployeddad@gmail.com>
2018-07-20 14:50:29 -0700

    done

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,21 @@
 // declare your functions here...
-function paragraphSelector(){
+function paragraphSelector() {
   return $('p');
 }
 
-function lastImageSelector(){
+function lastImageSelector() {
   return $('img:last');
 }
 
-function ninjaBabySelector(){
-  return $('#baby-ninja')
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child'); 
+  //use a first-child selector to return the first list item in the ul with the ID pic-list
 }
