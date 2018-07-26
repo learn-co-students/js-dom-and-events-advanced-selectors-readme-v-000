bf41ce11bb2789461005cc667ca1b4ffe464a508
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-26 16:11:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('.pics img:last');
+}
+
+function ninjaBabySelector() {
+  return $('.pics #baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
