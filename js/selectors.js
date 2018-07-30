9b25693cf20f6d888e713c87ba0abefd368e6ac9
github username <github email address>
2018-07-30 15:45:25 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
-// declare your functions here...
+// $(document).ready(function() {
+//
+//
+//
+//
+// });
+
+function paragraphSelector(){
+  return $('p')
+}
+
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+
+function ninjaBabySelector(){
+  return $('img#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
