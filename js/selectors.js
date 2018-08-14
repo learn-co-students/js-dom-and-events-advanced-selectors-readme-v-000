340cbc15db67255f71d711e6a59a4360557f820a
DGMarkel <davidgmarkel@gmail.com>
2018-08-14 15:39:56 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $("img[id='baby-ninja']");
+}
+
+function divSelector() {
+  return $('div:first-child, :last')
+}
+
+function firstListItem() {
+  return $('li:first-child')
+}
