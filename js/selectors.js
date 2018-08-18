4f68d14a3abb38a2295942c290edf717af350c61
ktamoguis <ktamoguis@gmail.com>
2018-08-18 19:53:30 +0800

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p');
+}
+
+function lastImageSelector(){
+  $('img:last');
+}
+
+function ninjaBabySelector (){
+  $('#baby-ninja');
+}
+
+function divSelector(){
+  $('div.pics');
+}
+
+function firstListItem(){
+  //$('parent-tag child-tag:first-child')
+  $('ul#pic-list li:first-child');
+}
