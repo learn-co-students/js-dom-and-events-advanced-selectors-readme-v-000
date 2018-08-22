5401e4e7bfd8d4133c7f1bf2f7dca42bb641fb19
github username <github email address>
2018-08-22 18:12:49 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+function paragraphSelector() {
+  // I need to return an array but my code returns undefined
+  return $('p');
+}
+
+function lastImageSelector() {
+  // return the last image
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  // return the image of the ninja baby
+  return $("img[id='baby-ninja']");
+}
+
+function divSelector() {
+  // should return two divs
+  return $('div[class]');
+}
+
+function firstListItem() {
+  // Return the first list item in the unordered list
+  // $('ul img:first-child') return an array of length 3
+  // return $('ul[id="pic-list"] img:first-child');
+  return $('ul li').first();
+}
