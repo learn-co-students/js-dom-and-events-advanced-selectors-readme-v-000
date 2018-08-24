970f44a230eac68ed04b4937ef9f985d733aa53f
Eugene <yujunsung@gmail.com>
2018-08-24 16:08:32 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+function paragraphSelector(){
+
+  return $('p');
+
+
+}
+function lastImageSelector(){
+
+  return $('img').last();
+
+}
+function ninjaBabySelector(){
+
+  return $('img#baby-ninja');
+
+}
+
+function divSelector(){
+
+  return $('div').splice(0,2);
+
+}
+
+function firstListItem(){
+
+  return $('ul li').first();
+}
