d4751134a1097f65eb4cd44424fdcc207448b7e1
Dillorom <dabdulloeva@gmail.com>
2018-08-28 16:00:25 -0700

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector(){
+    return document.getElementsByTagName('p')
+    //return $('p')  alternative
+}
+
+function lastImageSelector(){
+    return $('img:last')
+}
+
+function ninjaBabySelector(){
+    return $("#baby-ninja")
+}
+
+function divSelector(){
+    return $(".pics")
+}
+
+function firstListItem(){
+   return $('ul li:first-child')
+}
+
