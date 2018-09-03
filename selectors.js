7bc14f00acac905fe2cdfbbbad597c08a8176a93
github username <github email address>
2018-09-03 15:45:54 -0500

    unable to locate repo in file list

--- selectors.js
+++ selectors.js
@@ -6,9 +6,9 @@
 // $("img[alt='the beatles making faces']");
 // $('div:last');
 function paragraphSelector() {
- 	$('p').show;
-
-}
+ 	return $('p');
+ }
+	// document.paragraphSelector();
 
 function lastImageSelector() {
 	$('div:last'); // use the last jQuery selector to return the last image in index.html
@@ -26,3 +26,23 @@ function firstListItem() {
 	$('div-pic-list:first-child');  // use a first-child selector to retun the first list item in the ul with the ID pic-list
 }
 // declare your functions here...
+
+// <script type="text/javascript">
+
+//   function globalEval(data) {
+//     data = data.replace(/^\s+|\s+$/g, "");
+
+//     if (data) {
+//       var head = document.getElementsByTagName("head")[0] ||
+//               document.documentElement,
+//           script = document.createElement("script");
+
+//       script.type = "text/javascript";
+//       script.text = data;
+
+//       head.insertBefore(script, head.firstChild);
+//       head.removeChild(script);
+//     }
+//   }
+
+// </script>
