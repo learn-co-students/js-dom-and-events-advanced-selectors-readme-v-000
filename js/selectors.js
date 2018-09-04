6cc918de280af8d7718110462b4a968bdabcf572
jebediahelliott <jebediahelliott@gmail.com>
2018-09-04 05:02:19 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p')
+}
+
+function lasImagaeSelector() {
+  $('img:last')
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('ul li:first-child')
+}
