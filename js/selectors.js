28c44f1499300489315bf10a282cb1a3d09a0e27
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-04 09:06:58 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p')
+}
+ function lasImagaeSelector() {
+  $('img:last')
+}
+ function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+ function divSelector() {
+  $('.pics')
+}
+ function firstListItem() {
+  $('ul li:first-child')
+}
