d1d949fb6f58ce88280c4a558a517235b2ae52ce
peterckim <peterckim96@gmail.com>
2018-09-21 19:55:16 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+function paragraphSelector() {
+  let paragraphElements = $("p");
+
+  return paragraphElements;
+}
+
+function lastImageSelector() {
+  let lastImage = $("img:last");
+
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  let ninjaBabyImage = $("img#baby-ninja");
+
+  return ninjaBabyImage;
+}
+
+function divSelector() {
+  let divs = $("div.pics");
+
+  return divs;
+}
+
+function firstListItem() {
+  let firstListElement = $("ul#pic-list li:first-child");
+
+  return firstListElement;
+}
