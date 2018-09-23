1b205a597a49019f08281b87a1f99038fc9c0bb4
swgoodman <spencerwgoodman@icloud.com>
2018-09-23 15:15:34 -0700

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  const ps = $('p');
+  return ps;
+}
+
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
