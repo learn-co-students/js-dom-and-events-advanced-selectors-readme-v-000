5fc0606dd9273e353a03ef00e3552b5badcf6101
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-23 19:55:31 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+
+// $('img');                                   // ELEMENT SELECTOR
+// $('.pics');                                 // CLASS SELECTOR
+// $('#baby-ninja');                           // ID SELECTOR
+// $('ul li');                                 // DESCENDANT SELECTOR
+// $('parent-tag child-tag:first-child');      // FIRST CHILD SELECTOR SYNTAX
+// $('div img:first-child');                   // FIRST CHILD SELECTOR
+// $("img[alt='the beatles making faces']");   // ALT TAG SELECTOR
+// $('div:last');                              // LAST SELECTOR
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul li:first-child');
+}
