8f9dc0e6c691fbecdc9c4819dd9f3f3ec28e9655
epic0448 <samuelogundiran@Yahoo.com>
2019-01-26 14:33:03 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,37 @@
 // declare your functions here...
+
+function paragraphSelector(){
+return $('p')
+}
+
+function lastImageSelector(){
+ return $('img:last');
+}
+
+
+function ninjaBabySelector(){
+    return $('#baby-ninja')
+
+}
+
+ function diveSelector(){
+    return $('div.pics');
+   }
+
+function lastImageSelector(){
+    return $('.pics');
+}
+
+
+
+
+function firstListItem(){
+    return $('#pic-list ul li:first-child')
+}
+
+
+// $('ul li')
+// $('parent-tag child-tag:first-child');
+// $('div img:first-child')
+// $("img[alt='the beatles making faces']");
+// $('div:last');
