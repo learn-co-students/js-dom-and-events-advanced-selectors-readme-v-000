40cb2990fc0470f5b9aef3de1d61036650a6547f
Erin Green <erin.y.green@gmail.com>
2019-05-22 18:59:39 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraghSelector(){
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+
+}
+
+fucntion divSelector() {
+
+}
+
+function firstListItem() {
+
+}
