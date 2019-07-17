e146fde63d79c42ab88494cddab5b9e7724b1bc2
github username <github email address>
2019-07-17 16:39:12 -0400

    Done.

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-$(document).ready(function() {
-  // code to be executed goes here
-  <script src="node_modules/mocha/mocha.js"></script>
-  <script src="node_modules/expect/umd/expect.min.js"></script>
-  <script>mocha.setup('bdd');</script>
-  <script src="test/index-test.js"></script>
-});
