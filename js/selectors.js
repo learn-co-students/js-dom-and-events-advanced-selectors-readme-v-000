b8385620401f2596eef6c3d0d5be19f9a008e767
Heshie Brody <4hbrody@gmail.com>
2019-08-05 21:47:57 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -17,9 +17,3 @@ function divSelector () {
 function firstListItem () {
   return $("#pic-list li:first-child");
 }
-
-// Write a function divSelector that does not accept any parameters.
-// The function should use a class selector to return the two divs with the class pics.
-//
-// Write a function firstListItem that does not accept any parameters.
-// The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
